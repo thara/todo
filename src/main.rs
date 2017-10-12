1ffe3f2ba4ebacaabeaf318bbbb4430f764aea31
Tomochika Hara <hara@wonderpla.net>
2017-10-12 21:35:05 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-extern crate todo;
+use std::env;
 
-use todo::parser::task;
+extern crate todo;
+use todo::parser::{Task, task};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        eprintln!("Usage : todo {{description}}");
+        return;
+    }
+
     let line = "x (A) 2011-03-02 2011-03-01 Review Tim's pull request +TodoTxtTouch @github";
     let t = task(line).unwrap();
 
