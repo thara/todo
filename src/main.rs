54a1b8a07a12337211865f7878f91774680aedec
Tomochika Hara <tomochikahara@gmail.com>
2017-10-19 00:40:39 +0900

    Implement del subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::io;
 use std::io::prelude::*;
-use std::io::{BufReader, LineWriter};
+use std::io::{BufWriter, BufReader, LineWriter};
 use std::path::PathBuf;
 use std::ffi::OsStr;
 use std::os::unix::fs::PermissionsExt;
@@ -14,8 +14,10 @@ use std::os::unix::process::CommandExt;
 extern crate log;
 extern crate clap;
 extern crate ansi_term;
+extern crate tempfile;
 use ansi_term::ANSIString;
 use ansi_term::Colour::{Red, Yellow, Blue};
+use tempfile::NamedTempFile;
 
 extern crate todo;
 mod cli;
@@ -69,6 +71,38 @@ fn main() {
             w.write(bytes).expect("Writing bytes is failed");
             w.write(b"\n").expect("Writing bytes is failed");
         }
+        ("del", Some(sub_m)) => {
+            let task_no = sub_m.value_of("TASK_NO").unwrap();
+            let task_no = task_no.parse::<usize>().unwrap();
+
+            let f = BufReader::new(OpenOptions::new()
+                .read(true)
+                .open(txt_path)
+                .expect("todo.txt open failed"));
+
+            let tmpfile = NamedTempFile::new().ok().unwrap();
+            let tmpfile_path = tmpfile.path().to_owned();
+            let mut tmp = BufWriter::new(tmpfile);
+
+            for (i, line) in f.lines().filter_map(|e| e.ok()).enumerate() {
+                if i != task_no {
+                    let n = if task_no < i {
+                        i - 1
+                    } else {
+                        i
+                    };
+                    format_line(n, &line);
+                    tmp.write(line.as_bytes()).expect("Writing bytes is failed");
+                    tmp.write(b"\n").expect("Writing bytes is failed");
+                } else {
+                    print!("DELETE => ");
+                    format_line(i, &line);
+                }
+            }
+            tmp.flush().expect("Flush for tmpfile is failed");
+
+            fs::rename(tmpfile_path, txt_path).expect("Renaming file is failed");
+        }
         ("edit", Some(_)) => {
             let path: &OsStr = txt_path.as_ref();
             process::Command::new("vi").arg(path).exec();
@@ -77,27 +111,27 @@ fn main() {
             if metadata.len() == 0 {
                 println!("todo.txt is empty.");
             } else {
-                let f = OpenOptions::new().read(true).open(txt_path).expect("todo.txt open failed");
-                let reader = BufReader::new(f);
-                for (i, line) in reader.lines().enumerate() {
-                    match line {
-                        Ok(s) => {
-                            if let Some(task) = todo::parser::task(&s) {
-                                let t = format!("#{} {}", i, task);
-
-                                match task.priority {
-                                    Some(todo::Priority::High) => println!("{}", Red.paint(t)),
-                                    Some(todo::Priority::Mid) => println!("{}", Yellow.paint(t)),
-                                    Some(todo::Priority::Low) => println!("{}", Blue.paint(t)),
-                                    _ => println!("{}", ANSIString::from(t)),
-                                }
-
-                            }
-                        }
-                        _ => {}
-                    }
+                let f = BufReader::new(OpenOptions::new()
+                    .read(true)
+                    .open(txt_path)
+                    .expect("todo.txt open failed"));
+                for (i, line) in f.lines().filter_map(|e| e.ok()).enumerate() {
+                    format_line(i, &line);
                 }
             }
+        }
+    }
+}
+
+fn format_line(i: usize, s: &str) {
+    if let Some(task) = todo::parser::task(&s) {
+        let t = format!("{} {}", i, task);
+
+        match task.priority {
+            Some(todo::Priority::High) => println!("{}", Red.paint(t)),
+            Some(todo::Priority::Mid) => println!("{}", Yellow.paint(t)),
+            Some(todo::Priority::Low) => println!("{}", Blue.paint(t)),
+            _ => println!("{}", ANSIString::from(t)),
         }
     }
 }
